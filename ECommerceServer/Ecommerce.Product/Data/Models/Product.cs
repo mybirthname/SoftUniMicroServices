647d736a043e891791cdc2df8f10548b9b500350
Martin Stanchev <martin.stanchev87@gmail.com>
2020-07-04 11:59:32 +0300

    Add default projects

---
+++ ECommerceServer/Ecommerce.Product/Data/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Product.Data.Models
+{
+    public class Product
+    {
+        public Guid ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string NrIntern { get; set; }
+
+        public decimal PricePerPQ { get; set; }
+
+        public string URL { get; set; }
+
+        public string Description { get; set; }
+
+        public Guid SupplierID { get; set; }
+    }
+}
---
+++ ECommerceServer/Ecommerce.Product/Data/ProductDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Product.Data
+{
+    public class ProductDbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
