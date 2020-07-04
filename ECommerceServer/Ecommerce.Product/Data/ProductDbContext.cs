f76eb2a30aef0af4c107cb2242630c7e38d9f945
Martin Stanchev <martin.stanchev87@gmail.com>
2020-07-04 12:03:05 +0300

    Fix build erorr

--- ECommerceServer/Ecommerce.Product/Data/ProductDbContext.cs
+++ ECommerceServer/Ecommerce.Product/Data/ProductDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Ecommerce.Product.Data
